708383c0033aed00dd5bb2affc55d383f7f7e159
Joy <joy.macharia@student.moringaschool.com>
2023-06-23 13:02:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let newArr = [];
+
+    for (let i = 0; i < names.length; i++) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArr;
+}
+
+function countDown(positiveInt) {
+    let i = positiveInt;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
